8514bccf9c5c186c317c8584548e326823d573d5
oanacodes <oana.carlescu@gmail.com>
2019-09-26 17:27:54 +0200

    created teams

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 // const db = require('./db')
 // const Team = require('./team/model')
 const teamRouter = require('./team/router')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const port = process.env.PORT || 4002
 
+const jsonParser = bodyParser.json()
+
+app.use(jsonParser)
 app.use(teamRouter)
 
 app.listen(port, () => {
--- team/router.js
+++ team/router.js
@@ -11,4 +11,13 @@ router.get('/team', (req, res, next) => {
       .catch(console.error)
 })
 
+router.post('/team', (req, res, next) => {
+  Team
+    .create(req.body)
+    .then((team) => {
+      res.status(200).send(team)
+    })
+    .catch(next)
+})
+
 module.exports = router
