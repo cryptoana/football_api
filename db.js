22cd7bf717698a3910fb08899c175f4c00c72b2c
oanacodes <oana.carlescu@gmail.com>
2019-09-26 16:43:37 +0200

    created Heroku DB file

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+// DB url created by Heroku
+const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+
+const db = new Sequelize(databaseURL)
+
+db
+  .sync()
+  .then(() => console.log('DB updated'))
+  .catch(console.error)
+
+module.exports = db
